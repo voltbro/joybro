9eba4ad87cef8540cce36ec558d8c27e6f79355d
Ubercola <cola@cactus.ru>
2019-02-21 18:17:00 +0300

    Add python cmd_vel sample and launch files

---
+++ src/joy_teleop.py
@@ -0,0 +1,45 @@
+#! /usr/bin/python
+
+import rospy
+
+from geometry_msgs.msg import Twist
+from joybro.msg import JoyBro
+
+class JoyBroTeleop():
+
+    def __init__(self):
+        
+        rospy.init_node('joy_bro_teleop', anonymous=True)
+        
+        self.max_linear_vel = rospy.get_param('~max_linear_vel',0.25)
+        self.max_angular_vel = rospy.get_param('~max_angular_vel',2)
+        self.threshold = rospy.get_param('~threshold',20)
+        
+        self.sub = rospy.Subscriber("/joybro", JoyBro, self.callback)
+        self.pub = rospy.Publisher("/cmd_vel", Twist, queue_size=5)
+
+    def callback(self, data):
+
+        linear_vel = 0
+        angular_vel = 0
+
+        if abs(data.left_y) > self.threshold:
+            linear_vel = (data.left_y/512.0)*self.max_linear_vel
+
+        if abs(data.left_x) > self.threshold:    
+            angular_vel = (data.left_x/512.0)*self.max_angular_vel
+
+        twist = Twist()
+        twist.linear.x = linear_vel
+        twist.angular.z = angular_vel
+        self.pub.publish(twist)
+
+
+    def run(self):
+        rospy.loginfo("Start JoyBro Teleop")
+        rospy.spin()
+
+
+if __name__ == '__main__':
+    teleop = JoyBroTeleop()
+    teleop.run();
