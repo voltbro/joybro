0f58429c57d2e3135ffe2f69524a9f51abfe2ef2
Sorokonog <roman@voltbro.com>
2021-10-13 16:33:05 +0300

    limits adjusted

--- src/dog_teleop.py
+++ src/dog_teleop.py
@@ -12,13 +12,14 @@ class DogTeleop():
 
         self.state = 'stop'
         
-        self.max_linear_vel = rospy.get_param('~max_linear_vel',0.25)
-        self.max_angular_vel = rospy.get_param('~max_angular_vel',2)
+        self.max_linear_vel = rospy.get_param('~max_linear_vel',0.5)
+        self.max_angular_vel = rospy.get_param('~max_angular_vel',1.5)
         self.threshold = rospy.get_param('~threshold',20)
+        self.max_height = rospy.get_param('~max_height',0.5)
         
         self.sub = rospy.Subscriber("/joybro", JoyBro, self.callback)
         self.pub = rospy.Publisher("/cmd_vel", Twist, queue_size=5)
-        self.pub = rospy.Publisher("/rc_mode", Int16, queue_size=5)
+        self.pub_rc = rospy.Publisher("/rc_mode", Int16, queue_size=5)
 
 
     def callback(self, data):
@@ -26,19 +27,28 @@ class DogTeleop():
         linear_vel_x = 0
         angular_vel_z = 0
         linear_vel_y = 0 
+        rc_mode = Int16()
 
-        if self.state == 'stop' and data.btn3 == 1:
-            self.state = 'move'
+        if data.sw3:
+            self.state = "stand"
+            rc_mode.data = 3
+            self.pub_rc.publish(rc_mode)
 
-        if(self.state == 'move' and data.btn3 == 0):
-            twist = Twist()
-            self.pub.publish(twist)
-            self.state = 'stop'
-
-        if (self.state == "move" or self.state == "stop" and data.btn2 == 1):
-            twist = Twist()
-            self.pub.publish(twist)
-            self.state == "stand"
+        if data.sw2:
+            self.state = "move"
+            rc_mode.data = 11
+            self.pub_rc.publish(rc_mode)
+        
+        if data.sw1:
+            self.state = "stop"
+            rc_mode.data = 2
+            self.pub_rc.publish(rc_mode)
+        
+        if data.btn1:
+            self.state = "stop"
+            rc_mode.data = 12
+            self.pub_rc.publish(rc_mode)
+        
 
         if abs(data.left_y) > self.threshold:
             linear_vel_x = (data.left_y/512.0)*self.max_linear_vel
@@ -47,25 +57,26 @@ class DogTeleop():
             angular_vel_z = -(data.left_x/512.0)*self.max_angular_vel
 
         if abs(data.right_x) > self.threshold:    
-            linear_vel_y = -(data.left_x/512.0)*self.max_angular_vel
+            linear_vel_y = -(data.right_x/512.0)*self.max_angular_vel
+        
+        height = -((512.0 - data.slider1)/512.0)*self.max_height
 
-
-        if (self.state == 'move'):
+        if (self.state == 'stand'):
+            twist = Twist()
+            twist.angular.y = linear_vel_x
+            twist.angular.x = linear_vel_y/4
+            twist.angular.z = angular_vel_z/5
+            twist.linear.z = height
+            self.pub.publish(twist)
+        else:
             twist = Twist()
             twist.linear.x = linear_vel_x
             twist.linear.y = linear_vel_y
             twist.angular.z = angular_vel_z
-            twist.linear.z = data.slider1 #* K #TBD!!!!
+            twist.linear.z = height 
             self.pub.publish(twist)
 
-        if (self.state == 'move'):
-            twist = Twist()
-            twist.angular.x = linear_vel_x
-            twist.angular.y = linear_vel_y
-            twist.angular.z = angular_vel_z
-            twist.linear.z = data.slider1 #* K #TBD!!!!
-            self.pub.publish(twist)
-
+        
     def run(self):
         rospy.loginfo("Start Dog Teleop")
         rospy.spin()
